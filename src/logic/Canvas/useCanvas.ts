310f6b565c306a190b53119cf6ec4e1adf486317
Nevermore <15972699417@163.com>
2024-05-24 14:34:09 +0800

    更新leafer框架

--- src/logic/Canvas/useCanvas.ts
+++ src/logic/Canvas/useCanvas.ts
@@ -1,26 +1,17 @@
 import { watch } from 'vue'
 import { storeToRefs } from 'pinia'
-import { Canvas, Object as FabricObject, Textbox, Group, Point, IText, Line } from 'fabric'
-import { WorkSpaceThumbType, WorkSpaceDrawType } from "@/configs/canvas"
-import { useFabricStore } from '@/store/modules/fabric'
+import { Leafer, App, Rect } from 'leafer-ui'
 import { useElementBounding } from '@vueuse/core'
-import { FabricTool } from '@/app/fabricTool'
-import { FabricGuide } from '@/app/fabricGuide'
-import { HoverBorders } from '@/app/hoverBorders'
-import { WheelScroll } from '@/app/wheelScroll'
-import { FabricRuler } from '@/app/fabricRuler'
 import { isMobile } from '@/utils/common'
 import { FabricCanvas } from '@/app/fabricCanvas'
-import { Keybinding } from '@/app/keybinding'
-import { defaultControls, textboxControls } from '@/app/fabricControls'
-import { useTemplatesStore } from '@/store'
+import { useTemplatesStore, useLeaferStore } from '@/store'
 import useCommon from './useCommon'
 import useHammer from './useHammer'
 
 
 
 
-let canvas: null | FabricCanvas = null
+let app: null | App = null
 
 // 初始化配置
 const initConf = () => {
@@ -29,11 +20,13 @@ const initConf = () => {
 
 // 更新视图区长宽
 const setCanvasTransform = () => {
-  if (!canvas) return
-  // const fabricStore = useFabricStore()
-  // const { zoom, wrapperRef, scalePercentage } = storeToRefs(fabricStore)
-  // const { width, height } = useElementBounding(wrapperRef.value)
-  // canvas.setDimensions({width: width.value, height: height.value})
+  if (!app) return
+  const leaferStore = useLeaferStore()
+  const { zoom, wrapperRef, scalePercentage } = storeToRefs(leaferStore)
+  const { width, height } = useElementBounding(wrapperRef.value)
+  // canvas.set({width: width.value, height: height.value})
+  // canvas.width = width.value
+  // canvas.height = height.value
   // const objects = canvas.getObjects().filter(ele => !WorkSpaceThumbType.includes(ele.id))
   // const boundingBox = Group.prototype.getObjectsBoundingBox(objects)
   // if (!boundingBox) return
@@ -52,50 +45,65 @@ const setCanvasTransform = () => {
 }
 
 const initCanvas = () => {
-  const fabricStore = useFabricStore()
-  const { canvasRef } = storeToRefs(fabricStore)
-  const fabricWidth = fabricStore.getWidth()
-  const fabricHeight = fabricStore.getHeight()
+  const leaferStore = useLeaferStore()
+  const { canvasRef } = storeToRefs(leaferStore)
+  const leaferWidth = leaferStore.getWidth()
+  const leaferHeight = leaferStore.getHeight()
   if (!canvasRef.value) return
-  canvas = new FabricCanvas(canvasRef.value, {
-    width: fabricWidth,
-    height: fabricHeight
-  })
+  // canvas = new Leafer({
+  //   view: canvasRef.value, 
+  //   width: leaferWidth,
+  //   height: leaferHeight
+  // })
+  app = new App({ 
+    view: canvasRef.value,
+    ground: { type: 'draw' },
+    tree: {},
+    sky: { type: 'draw' }
+})
+
+  const background = new Rect({ width: 800, height: 600, fill: 'gray' })
+  const rect = new Rect({ x: 100, y: 100, fill: '#32cd79', draggable: true })
+  const border = new Rect({ x: 200, y: 200, stroke: 'blue', draggable: true })
+
+  app.ground.add(background)
+  app.tree.add(rect)
+  app.sky.add(border)
   // const keybinding = new Keybinding()
-  new FabricTool(canvas)
-  new FabricGuide(canvas)
-  new HoverBorders(canvas)
-  new WheelScroll(canvas)
-  new FabricRuler(canvas)
-  canvas.preserveObjectStacking = true
-  canvas.renderAll()
+  // new FabricTool(canvas)
+  // new FabricGuide(canvas)
+  // new HoverBorders(canvas)
+  // new WheelScroll(canvas)
+  // new FabricRuler(canvas)
+  // canvas.preserveObjectStacking = true
+  // canvas.renderAll()
 }
 
 const initEvent = () => {
-  if (!canvas) return
-  const templatesStore = useTemplatesStore()
-  canvas.on('object:modified', () => templatesStore.modifedElement())
+  if (!app) return
+  // const templatesStore = useTemplatesStore()
+  // canvas.on('object:modified', () => templatesStore.modifedElement())
 }
 
 // 初始化模板
 const initTemplate = async () => {
-  if (!canvas) return
-  const templatesStore = useTemplatesStore()
-  const { initCommon } = useCommon()
-  const { initHammer } = useHammer()
-  const { currentTemplate } = storeToRefs(templatesStore)
-  await canvas.loadFromJSON(currentTemplate.value)
+  if (!app) return
+  // const templatesStore = useTemplatesStore()
+  // const { initCommon } = useCommon()
+  // const { initHammer } = useHammer()
+  // const { currentTemplate } = storeToRefs(templatesStore)
+  // await canvas.loadFromJSON(currentTemplate.value)
   setCanvasTransform()
-  initCommon()
-  initEvent()
-  if (isMobile()) {
-    initHammer()
-  }
+  // initCommon()
+  // initEvent()
+  // if (isMobile()) {
+  //   initHammer()
+  // }
 }
 
 export const initEditor = async () => {
-  const fabricStore = useFabricStore()
-  const { wrapperRef } = storeToRefs(fabricStore)
+  const leaferStore = useLeaferStore()
+  const { wrapperRef } = storeToRefs(leaferStore)
   initConf()
   initCanvas()
   initTemplate()
@@ -105,4 +113,4 @@ export const initEditor = async () => {
   })
 }
 
-export default (): [FabricCanvas] => [canvas as FabricCanvas]
+export default (): [App] => [app as App]
